58652af9e8989190050412d30c81d8c04b58f0b1
gi-os <gio@gi-os.com>
2017-01-12 09:36:25 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+function opengios6() {
+  location.href='http://gi-os6.tk/';
+}
+function opengalileo() {
+  location.href='http://galileo.ml/';
+}
+function openedge() {
+  location.href='http://www.gi-os.com/#!edge/c7b9';
+}
+function opengrid() {
+  location.href='http://www.gi-os.com/#!join-grid/cq6l';
+}
+function openspotlight() {
+  location.href='http://www.gi-os.com/#!revolution/c1mpu';
+}
+function opendashboard() {
+  location.href='http://www.gi-os.com/#!launch/mainPage';
+}
+function opencodepen() {
+  location.href='http://codepen.io/gi-os/';
+}
+function openmiku3() {
+  location.href='http://s.codepen.io/gi-os/debug/GNKyvG';
+}
+function openmiku2() {
+  location.href='http://s.codepen.io/gi-os/debug/ZWQMxa';
+}
+function openmiku1() {
+  location.href='http://s.codepen.io/gi-os/debug/GNKyvG';
+
+}  
+function opengiosi() {
+  location.href='http://s.codepen.io/gi-os/debug/jrRxAy';
+
+}  
+function opengithub() {
+  location.href='https://github.com/gi-os/';
+
+}  
