f04b43d2f3d22cdd74b79153f03aff48699c7d92
gi-os <gio@gi-os.com>
2017-01-12 09:52:47 -0500

    Create I.js

---
+++ projects/I.js
@@ -0,0 +1,45 @@
+//Thanks to Hide My Ass for suplying the servers needed to run this
+function goBack() {
+ window.history.back();
+        }
+function goForward() {
+ window.history.forward();
+        }
+function goHome() {
+ window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+  window.history.back();
+        }
